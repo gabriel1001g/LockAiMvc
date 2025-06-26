7986bf1fcab6ad51a57dfbc577934583336e4ca3
gabriel1001g <gabrielsg2000@gmail.com>
2025-06-25 22:03:08 -0300

    Front comunicando com API no Somee

--- Controllers/ObjetoController.cs
+++ Controllers/ObjetoController.cs
@@ -1,77 +1,150 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using LockaiMvc.Models;
 using Microsoft.AspNetCore.Mvc;
-using LockaiMvc.Models;
-using System.Net.Http;
 using Newtonsoft.Json;
 
 namespace LockaiMvc.Controllers
 {
-    
-    public class ObjetoController : ControllerBase
+    public class ObjetoController : Controller
     {
-        public string uriBase = "";
+        public string uriBase = "http://guigao.somee.com/TccApi/Objeto/";
 
-    }
-
-    [HttpGet]
-    public ActionResult Index()
-    {
-        return View("CadastrarObjeto");
-    }
-
-   
-    [HttpPost]
-    public async Task<ActionResult> RegistrarAsync(ObjetoViewModel u)
-    {
-        try
+       [HttpGet]
+        public async Task<ActionResult> IndexAsync()
         {
-            HttpClient httpClient = new HttpClient();
-            string uriComplementar = "Registrar";
-
-            var content = new StringContent(JsonConvert.SerializeObject(u));
-            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("Application/json");
-            HttpResponseMessage response = await httpClient.PostAsync(uriBase + uriComplementar, content);
-
-            string serialized = await response.Content.ReadAsStringAsync();
-
-            if(response.StatusCode == System.Net.HttpStatusCode.OK)
+            try
             {
-                TempData["Mensagem"] = string.Format("Objeto {0 Registrado com sucesso! Faça o login para acessar."u.00);
-                return View("AutenticarObjeto");
+                string uriComplementar = "GetAll";
+                HttpClient httpClient = new HttpClient();
+                HttpResponseMessage response = await httpClient.GetAsync(uriBase + uriComplementar);
+
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    List<ObjetoViewModel> ConsultarObjeto = await Task.Run(() =>
+                    JsonConvert.DeserializeObject<List<ObjetoViewModel>>(serialized)); 
+                    return View("ConsultarObjeto", ConsultarObjeto);
+                }
+                else
+                {
+                    throw new System.Exception($"Erro ao carregar objetos: {serialized}");
+                }
             }
-            else
+            catch (System.Exception ex)
             {
-                throw new System.Exception(serialized);
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
             }
         }
-        catch(System.Exception ex)
+/*
+        [HttpPost]
+        public async Task<ActionResult> RegistrarAsync(ObjetoViewModel u)
         {
-            TempData["MensagemErro"] = ex.Message;
-            return RedirectToAction("Index");
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string uriComplementar = "/Registrar";
+
+                var content = new StringContent(JsonConvert.SerializeObject(u));
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                HttpResponseMessage response = await httpClient.PostAsync(uriBase + uriComplementar, content);
+
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    TempData["Mensagem"] = string.Format("Objeto {0} registrado com sucesso! Faça o login para acessar.", u.Nome);
+                    return View("AutenticarObjeto");
+                }
+                else
+                {
+                    throw new System.Exception(serialized);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
         }
 
-    }
-     [HttpGet]
-    public ActionResult IndexLogin()
+        [HttpGet]
+        public ActionResult IndexLogin()
         {
-        return View("AutenticarObjeto");
+            return View("AutenticarObjeto");
         }
 
-    [HttpPost]
-    public async Task<ActionResult> AutenticarAsync(ObjetoViewModel u)
-    {
-        try
+/*
+        [HttpPost]
+        public async Task<ActionResult> AutenticarAsync(ObjetoViewModel u)
         {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string uriComplementar = "/Autenticar";
 
+                var content = new StringContent(JsonConvert.SerializeObject(u));
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                HttpResponseMessage response = await httpClient.PostAsync(uriBase + uriComplementar, content);
+
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    TempData["Mensagem"] = "Objeto autenticado com sucesso!";
+                    return RedirectToAction("ListarObjetos");
+                }
+                else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized || response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                {
+                    TempData["MensagemErro"] = "Credenciais inválidas. Por favor, tente novamente.";
+                    return View("AutenticarObjeto");
+                }
+                else
+                {
+                    throw new System.Exception(serialized);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return IndexLogin();
+            }
         }
-        catch(System.Exception ex)
+        */
+
+        [HttpGet]
+        public async Task<ActionResult> ListarObjetos()
         {
-            TempData["MensagemErro"] = ex.Message;
-            return IndexLogin();
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                HttpResponseMessage response = await httpClient.GetAsync(uriBase);
+
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    List<ObjetoViewModel> objetos = JsonConvert.DeserializeObject<List<ObjetoViewModel>>(serialized);
+                    return View("ListarObjetos", objetos);
+                }
+                else
+                {
+                    throw new System.Exception($"Erro ao carregar objetos: {serialized}");
+                }
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("IndexLogin");
+            }
         }
     }
 }
